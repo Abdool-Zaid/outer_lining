72d8e7785523ffe7f0fbec5c3b3a285af1ad9087
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-04-30 19:13:03 +0200

    update

--- script.js
+++ script.js
@@ -19,9 +19,11 @@ window.addEventListener('DOMContentLoaded',()=>{ // find the stuff that needs to
         
             if(Element.innerHTML.includes(pattern_start) && Element.innerHTML.includes(pattern_end))
                 if(Element.innerHTML.indexOf(pattern_start)< Element.innerHTML.indexOf(pattern_end)){
-                    // add to the list of things that should be rerendered
-                    get_variable_name(Element)
+                    let variable_name= get_variable_name(Element)
                     add_unique_id(Element,index)
+                    // add to the list of things that should be rerendered
+                    console.log(variable_name)
+
                 }
         }
     })
@@ -33,17 +35,16 @@ let get_variable_name = (Element)=>{
     let i_start = Element.innerHTML.indexOf(pattern_start) + pattern_start.length
     let i_end = Element.innerHTML.indexOf(pattern_end)
     let variable_name = Element.innerHTML.slice(i_start,i_end)
-    console.log(variable_name)
+    // console.log(variable_name)
+    return variable_name
 }
 
 let add_unique_id = (Element, number)=>{
     let uid = "v"
         uid = uid + number +  crypto.randomUUID()
-        console.log(uid)
-    // todo assign id to element
-
-    // return uid
+        Element.uid = uid
+        
 }
 
-// return the state for other scripts to acccess
+
  window.state = state
