e438c2d7014ea0351e7849322075c41929553ad0
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-04-30 14:55:44 +0200

    working on UID

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ let pattern_end = '}}'
 window.addEventListener('DOMContentLoaded',()=>{ // find the stuff that needs to change
     // console.log("content has been loaded")
    let all_elements = document.querySelectorAll('*')
-    all_elements.forEach(Element=>{
+    all_elements.forEach((Element,index)=>{
         if(!Element.innerHTML.includes("</")){
         
             if(Element.innerHTML.includes(pattern_start) && Element.innerHTML.includes(pattern_end))
                 if(Element.innerHTML.indexOf(pattern_start)< Element.innerHTML.indexOf(pattern_end)){
                     // add to the list of things that should be rerendered
                     get_variable_name(Element)
+                    add_unique_id(Element,index)
                 }
         }
     })
@@ -24,4 +25,13 @@ let get_variable_name = (Element)=>{
     let i_end = Element.innerHTML.indexOf(pattern_end)
     let variable_name = Element.innerHTML.slice(i_start,i_end)
     console.log(variable_name)
+}
+
+let add_unique_id = (Element, number)=>{
+    let uid = "v"
+        uid = uid + number +  crypto.randomUUID()
+        console.log(uid)
+    // todo assign id to element
+
+    // return uid
 }
