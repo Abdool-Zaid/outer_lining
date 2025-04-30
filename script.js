9f01d41370c4252148c7bc638cf9628df08bd5ea
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-04-30 14:35:11 +0200

    finds variables

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-window.addEventListener("load",()=>{
-    console.log("working")
+
+let pattern_start = '{{'
+let pattern_end = '}}'
+
+window.addEventListener('DOMContentLoaded',()=>{ // find the stuff that needs to change
+    // console.log("content has been loaded")
+   let all_elements = document.querySelectorAll('*')
+    all_elements.forEach(Element=>{
+        if(!Element.innerHTML.includes("</")){
+        
+            if(Element.innerHTML.includes(pattern_start) && Element.innerHTML.includes(pattern_end))
+                if(Element.innerHTML.indexOf(pattern_start)< Element.innerHTML.indexOf(pattern_end))
+            console.log( Element.innerHTML)
+        }
+    })
+
+
 })
+
