af4db808ec84ada79a48bba0bfacbb73510a6d71
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-05-06 22:36:11 +0200

    dummy proxy works

--- data.js
+++ data.js
@@ -4,11 +4,7 @@ console.log(state)
 
 let action_btn = document.getElementById('action_btn')
 action_btn.addEventListener('click', ()=>{
-     
-    state.variables.x_num == null ? state.variables.x_num = 1 : true
-    state.variables.x_num++
     
-    console.log(state.variables.x_num)
-
+    state.variables.x_num++
     
 })
--- proxy.js
+++ proxy.js
@@ -9,15 +9,38 @@ let _state = {
     locations:_locations,
     controllers: {}
 }
-const handler1= {}
-const state = new Proxy(_state, handler1);
+
+
+const handler = {
+    get(target, key) {
+        if (typeof target[key] === 'object' && target[key] !== null) {
+          return new Proxy(target[key], handler)
+        } else {
+          return target[key];
+        }
+      },
+      set (target, key, value) {
+        console.log(target)
+        if(key === 'x_num'){
+            console.log(key, 'key');
+            let x_num = document.getElementById('var_1')
+            x_num.innerHTML= value
+            target[key] = value;
+        }
+        return true
+      }
+};
+
+
+const state = new Proxy(_state, handler);
 
 
 
 document.addEventListener('DOMContentLoaded',()=>{
-    let x_num = document.getElementById('var_1')
     let y_num = document.getElementById('var_2')
-    console.log(x_num.innerHTML, y_num.innerHTML)
+    let x_num = document.getElementById('var_1')
+    x_num.innerHTML= state.variables.x_num
+    y_num.innerHTML= state.variables.y_num
 })
 
 
