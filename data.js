4545908b674890d476e711d2d9ce23f2147e43de
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-05-09 16:45:52 +0200

    proof of concept 1

--- data.js
+++ data.js
@@ -1,6 +1,9 @@
 
-console.log(state)
+console.log(state.controllers.wants_dark_theme(test_func))
 
+function test_func (){
+    console.log('test func')
+}
 
 let action_btn = document.getElementById('action_btn')
 action_btn.addEventListener('click', ()=>{
---
+++ proof_1.js
@@ -0,0 +1,65 @@
+let _state ={
+    data:{},
+    themes: {
+        light:{
+            primary: '',
+            secondary: '',
+            accent : '',
+        },
+        dark:{
+            primary: '#050808',
+            secondary: '#333535',
+            accent : '#ff8847',
+        },
+    },
+    _locations:[],
+    controllers:{
+        wants_dark_theme :(arg)=> _prefers_dark_theme(arg)
+    }
+}
+const _handler = {
+    get(target, key) {
+        // user defined  pre-hook
+        if (typeof target[key] === 'object' && target[key] !== null) {
+          return new Proxy(target[key], _handler)
+        } else {
+          return target[key];
+        }
+        // user defined post-hook
+      },
+      set (target, key, value) {// add hooks here as well
+            target[key] = value;
+        return true
+      }
+};
+
+
+
+
+
+function _prefers_dark_theme(func) { // might be better to put styling stuff in a seperate style file
+    if(typeof(func)== 'function'){
+        func()
+    }
+    return window.matchMedia('(prefers-color-scheme: dark)').matches
+  }
+
+function _set_theme(){
+const styles = document.createElement('style')
+const cssRules = `
+    *{
+        background-color: ${state.themes.dark.primary};
+        color:${state.themes.dark.secondary};
+    }
+`;
+
+styles.textContent = cssRules;
+document.head.appendChild(styles);
+
+}
+window.addEventListener('DOMContentLoaded',()=>{
+    _set_theme()
+})
+
+
+const state = new Proxy(_state, _handler);
