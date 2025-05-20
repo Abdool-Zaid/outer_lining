c4c1b8bad70ea292e9878e5a3a2e8a7a730cab1d
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-05-20 10:35:29 +0200

    prepping for merge

--- data.js
+++ data.js
@@ -9,6 +9,5 @@ let action_btn = document.getElementById('action_btn')
 action_btn.addEventListener('click', ()=>{
     
     state.data.x_num++
-    // state.inputs.foo = 'test string'
     // [state.data.y_num,state.data.x_num] = [state.data.x_num,state.data.y_num]
 })
